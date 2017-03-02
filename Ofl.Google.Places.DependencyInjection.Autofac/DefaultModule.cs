087021fb117725d16d7291e3fe81858270d2dba9
casperOne <casperOne@caspershouse.com>
2017-03-02 18:14:29 -0500

    Initial commit.

---
+++ Ofl.Google.Places.DependencyInjection.Autofac/DefaultModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Autofac;
+
+namespace Ofl.Google.Places.DependencyInjection.Autofac
+{
+    public class DefaultModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Validate parameters.
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            // Bind.
+            builder.RegisterType<PlacesClient>().As<IPlacesClient>();
+        }
+    }
+}
---
+++ Ofl.Google.Places.DependencyInjection.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ofl.Google.Places.DependencyInjection.Autofac")]
+[assembly: AssemblyDescription("Ofl.Google.Places.Contract implementation registrations in Autofac.")]
+[assembly: AssemblyProduct("Ofl.Google.Places.DependencyInjection.Autofac")]
